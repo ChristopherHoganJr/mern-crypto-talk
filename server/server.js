53caebb067553d8929f03bd9dcb62c4fc45b4773
chris hogan <christopherhoganjr@icloud.com>
2023-02-12 10:12:23 -0800

    forgot cookie parser. now added

--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const app = express();
+app.use(cookieParser());
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
