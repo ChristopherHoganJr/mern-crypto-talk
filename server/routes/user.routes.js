5e2299430c6b680a40b74d29fb9eecb558030cff
chris hogan <christopherhoganjr@icloud.com>
2023-02-12 10:09:05 -0800

    made users logout route authenticate

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -4,5 +4,5 @@ const { authenticate } = require("../config/jwt.config");
 module.exports = (app) => {
   app.post("/api/users/register", User.register);
   app.post("/api/users/login", User.login);
-  app.get("/api/users/logout", User.logout);
+  app.get("/api/users/logout", authenticate, User.logout);
 };
